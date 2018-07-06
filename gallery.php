9bd5fdfa97ac18bb0d798e9df9107d395ae5930e
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2018-07-06 16:42:56 +0530

    grid system for  4-3-2-1
     .col-xl-3 .col-lg-4 .col-sm-6"
    
    <div class="container">
      <div class="row">
        <div class="col-xl-3 col-lg-4 col-sm-6" >
    </div>
    </div>
    </div>

---
+++ gallery.php
@@ -0,0 +1,73 @@
+<?php
+// add header
+include 'include/header_main.php';
+?>
+<nav class="navbar navbar-toggleable-sm navbar-inverse bg-inverse">
+    <div class="container">
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+          <a href="index.php" class="navbar-brand">Doctor <span style="color:#20a7db;">JD</span></a>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+              <a href="index.php" class="nav-link">Home </a>
+          </li>
+
+          <li class="nav-item active">
+              <a href="about.php" class="nav-link">About Us </a>
+          </li>
+          <li class="nav-item ">
+              <a href="services.php" class="nav-link">Services </a>
+          </li>
+          <li class="nav-item">
+              <a href="blog.php" class="nav-link">blogs </a>
+          </li>
+          <li class="nav-item">
+              <a href="contact.php" class="nav-link">Contact </a>
+          </li>
+          <li class="nav-item">
+              <a href="gallery.php" class="nav-link active">Gallery</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Main SECTION -->
+  <header id="page-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 offset-md-3 text-center about-us-heading">
+          <h1 style="font-weight: 900;color:#20a7db; " class="display-2">Photo Gallery</h1>
+        </div>
+      </div>
+    </div>
+  </header>
+
+<div class="container">
+  <div class="row">
+    <div class="col-xl-3 col-lg-4 col-sm-6 " >
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="col-xl-3 col-lg-4 col-sm-6">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+    <div class="col-xl-3 col-lg-4 col-sm-6">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  <div class="col-xl-3 col-lg-4 col-sm-6">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="col-xl-3 col-lg-4 col-sm-6">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="col-xl-3 col-lg-4 col-sm-6">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</div>
+
+
+  <?php
+ // add footer
+ include 'include/footer_main.php';
+?>
